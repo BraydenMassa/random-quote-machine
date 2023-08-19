797f4d4881ec28a2d62542a03df2ec47b3623267
brayden <bmassa56@gmail.com>
2023-08-19 14:54:22 -0400

    quote machine first commit

---
+++ src/components/QuoteBox.js
@@ -0,0 +1,14 @@
+import '../styles/main.scss'
+
+const QuoteBox = () => {
+    return (
+    <div id="quote-box" className="App">
+      <div id="text"></div>
+      <div id="author"></div>
+      <button id="new-quote"></button>
+      <a id="tweet-quote"></a>
+    </div>
+    )
+}
+
+export default QuoteBox
